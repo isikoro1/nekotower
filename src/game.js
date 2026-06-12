7b6ca553b35fef06030c5a2f0b96a9566fb639df
isikoro1 <i.yb77kr@gmail.com>
2026-06-13 01:38:31 +0900

    Slow rotation acceleration and decay

--- src/game.js
+++ src/game.js
@@ -22,9 +22,9 @@ const HIT_SCALE = 0.9;
 const CAT_SCALE = 1.3;
 const SMALL_CAT_SCALE = 1.5;
 const SMALL_CONTOUR_AREA = 0.13;
-const ROT_ACCEL = 0.0012;
+const ROT_ACCEL = 0.0006;
 const ROT_MAX = 0.075;
-const ROT_IDLE_DECAY = 0.93;
+const ROT_IDLE_DECAY = 0.965;
 
 if (window.decomp) {
   Common.setDecomp(window.decomp);
