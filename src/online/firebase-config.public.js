fe83ab12743100bd9f796549198339dba7c5f65f
isikoro1 <i.yb77kr@gmail.com>
2026-06-28 00:06:13 +0900

    Add public Firebase config for online battle

---
+++ src/online/firebase-config.public.js
@@ -0,0 +1,9 @@
+window.NEKO_TOWER_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyBZ0_FAYUEyEIzkYOsxap40CxNK-uaJEUw",
+  authDomain: "nekotower-61266.firebaseapp.com",
+  databaseURL: "https://nekotower-61266-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "nekotower-61266",
+  storageBucket: "nekotower-61266.firebasestorage.app",
+  messagingSenderId: "913818156136",
+  appId: "1:913818156136:web:372828cd722887d2724fdd",
+};
